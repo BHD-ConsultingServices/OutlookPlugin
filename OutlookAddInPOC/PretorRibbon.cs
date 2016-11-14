5fa668cc54f8ef3bf3cf43d42cd7d17b00dda139
Ewald Van Niekerk <EwaldV@pretor.co.za>
2016-11-14 09:52:40 +0200

    First Commit

---
+++ OutlookAddInPOC/PretorRibbon.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookAddInPOC
+{
+    using System.Windows.Forms;
+    using Microsoft.Office.Interop.Outlook;
+
+    public partial class PretorRibbon
+    {
+        private void PretorRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook._Application oApp = new Outlook.Application();
+            if (oApp.ActiveExplorer().Selection.Count > 0)
+            {
+                Object selObject = oApp.ActiveExplorer().Selection[1];
+
+                if (selObject is Outlook.MailItem)
+                {
+                    Outlook.MailItem mailItem = (selObject as Outlook.MailItem);
+
+                    // Reads the body of the mail in HTML
+                    String htmlBody = mailItem.HTMLBody;
+
+                    // Reads the body of the mail in string
+                    String Body = mailItem.Body;
+
+                    int attachRestantes = mailItem.Attachments.Count;
+
+                    for (int j = attachRestantes; j >= 1; j--)
+                    {
+                        var attachementObject = mailItem.Attachments[j];
+                        if (attachementObject.BlockLevel == OlAttachmentBlockLevel.olAttachmentBlockLevelNone)
+                        {
+                            attachementObject.SaveAsFile(@"C:\temp\attachement" + attachementObject.FileName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            // Does some other work.
+        }
+    }
+}
---
+++ OutlookAddInPOC/ReadMe.txt
@@ -0,0 +1,29 @@
+﻿Outlook Plugin Spike
+
+This spike demonstrates how to create a simple plugin into Microsoft Outlook. In the plugin, we added two buttons. One that reads the email with all its contents. The second button is just a sample. 
+
+How to launch and operate:
+1.	Ensure your Outlook is closed.
+2.	Open the solution and start it.
+3.	It will launch Outlook with the plugin already installed.
+4.	Open an email.
+5.	You will see a new ribbon on top named “Add Tab Name Here”.
+6.	Once you click on it you will see the two buttons. 
+7.	The first one will show you how to access the email contents any attachment. You can also access attachments inside an attached email with recursion.
+8.	Have fun!
+
+Known Problems
+1.	Only works on Outlook 2013 & 2016. 
+2.	We also encountered issues in 2013. 
+3.	The certificate sometimes gives issues. Just install it under Trusted. Any work arounds here are welcome.
+
+Release Strategy
+1.	We deployed this via click-once. Just run the setup on the desired PC and every time Outlook is opened it check for a new update. 
+2.	Update are seamless then and the user don’t even see it. 
+
+Security
+1.	Because this application might live on remote machines, we don’t save any connection strings on anything on it. 
+2.	All data is passed to a web service.
+
+Best Regards 
+Pretor Team
